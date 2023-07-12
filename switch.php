cc0010226aed797f26bdb06624247541750679db
Ritika179 <139317244+Ritika179@users.noreply.github.com>
2023-07-11 20:31:06 -1000

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+$color = "bh";
+
+switch ($color){
+case"r";
+    echo"The color is red";
+break;
+    case"g";
+        echo"The color is green";
+    break;
+        case"b";
+            echo"The color is blue";
+        break;
+    default:
+          echo "not a valid color code";
+    }
+   ?>      
